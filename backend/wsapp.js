c907e5dbd0b7935bcd3c4adab48a369a56f1b9cf
fengyun2 <2816708306@qq.com>
2016-11-30 16:32:15 +0800

    更新html5 websocket示例和nodejs

--- backend/wsapp.js
+++ backend/wsapp.js
@@ -1,8 +1,22 @@
 const WebSocketServer = require('ws').Server,
     url = require('url'),
     wss = new WebSocketServer({
-        port: 8000
+        port: 8000, // 监听端口
+        verifyClient: socketVerify // 可选参数, 验证连接函数
     })
+
+function socketVerify (info) {
+    console.log(info.origin)
+    // console.log(info.req)
+    console.log(info.secure)
+    if (info.origin.indexOf('weixin')) {
+        // console.log(info.req.headers['user-agent'])
+        global.isWeixin = true
+    } else {
+        global.isWeixin = false
+    }
+    return true
+}
 
 // 广播
 wss.broadcast = function broadcast (type = 1, ws) {
@@ -37,10 +51,21 @@ wss.on('connection', function (ws) {
     // 接受信息/发送
     ws.on('message', function (json) {
         // console.log('received: %s ', message)
-        const obj = JSON.parse(json)
-        this.user = obj
-        if (typeof this.user.msg != 'undefined') {
-            wss.broadcast(1, obj)
+        try {
+            if (global.isWeixin) {
+                console.log(`received: %s`, json)
+            } else {
+                const obj = JSON.parse(json)
+                this.user = obj
+                if (typeof this.user.msg != 'undefined') {
+                    wss.broadcast(1, obj)
+                }
+
+            }
+
+        } catch (err) {
+            console.log('接受信息失败', err)
+            ws.send('接受信息失败', err)
         }
     })
     // 退出聊天
